60b879220749b58d7ad8bb92733d61c1b1c3f2f0
Warner Leijenaar <wleijenaar@hmc-heerema.com>
2022-08-09 21:07:43 +0200

    Added some documentation and refactoring

--- src/base/timer.py
+++ src/base/timer.py
@@ -17,21 +17,49 @@ from . import log
 
 
 class Timer(ContextDecorator):
+    """
+    Timer context decorator
+
+    ```py
+    from base import Timer
+    from time import sleep
+
+    @Timer('a message', logger)
+    def run_long_code():
+        sleep(30)
+    ```
+    """
     start: float
     logger: structlog.BoundLoggerBase
     msg: str = "Timed duration"
 
-    def __init__(self, msg: Optional[str] = None) -> None:
+    def __init__(
+        self,
+        msg: Optional[str] = None,
+        logger: Optional[structlog.BoundLoggerBase] = None
+    ) -> None:
+        self.__start(msg, logger)
+
+    def __start(
+        self,
+        msg: Optional[str] = None,
+        logger: Optional[structlog.BoundLoggerBase] = None,
+    ) -> None:
         if msg:
             self.msg = msg
 
-        self.logger = log.get_logger(self.__class__.__name__)
+        if isinstance(logger, structlog.BoundLoggerBase):
+            self.logger = logger
+        else:
+            self.logger = log.get_logger(self.__class__.__name__)
 
-    def __enter__(self, msg: Optional[str] = None) -> "Timer":
+    def __enter__(
+        self,
+        msg: Optional[str] = None,
+        logger: Optional[structlog.BoundLoggerBase] = None,
+    ) -> "Timer":
+        self.__start(msg, logger)
         self.start = time.perf_counter()
-
-        if msg:
-            self.msg = msg
 
         return self
 
